6b9332a17783b9e787a51b4b6953f3f782811de5
Samukaka01 <samuca.rib0109@gmail.com>
2024-09-06 13:32:37 -0300

    colocando um switch case para as marcas e categorias (falta código!!)

--- home/home.js
+++ home/home.js
@@ -1,11 +1,209 @@
+const inProduto = document.getElementById("inProduto");
 const slCategorias = document.getElementById("slCategorias");
 const slMarca = document.getElementById("slMarca");
 const inPreco1 = document.getElementById("inPreco1");
 const inPreco2 = document.getElementById("inPreco2");
 const btFiltrar = document.getElementById("btFiltrar");
 const outProdutos = document.getElementById("outProdutos");
-
+/*61 marcas diferentes switch case nelas (eu acho ne) --samuel */ 
 btFiltrar.addEventListener("click", filtrar);
 function filtrar(){
-
+    const categoria = slCategorias.value;
+    const marca = slMarca.value;
+    switch(categoria){
+        case "opMerc":
+            /*mostrar os produtos da categoria de mercearia*/
+            break;
+        case "opHort":
+            /*mostrar os produtos da categoria de hortifruti*/
+            break;
+        case "opProdHigLimp":
+            /*mostrar os produtos da categoria de higiene e limpeza */
+            break;
+        case "opCarne":
+            /*mostrar os produtos da categoria de carne*/
+            break;
+    }
+    switch(marca){
+        case"opMenorP":
+            /*marca respectiva*/
+            break;
+        case"opToddy":
+            /*marca respectiva*/
+            break;
+        case"opNescau":
+            /*marca respectiva*/
+            break;
+        case"opAltoAl":
+            /*marca respectiva*/
+            break;
+        case "opZaeli":
+            /*marca respectiva*/
+            break;
+        case "opTioJ":
+            /*marca respectiva*/
+            break;
+        case "opCocamar":
+            /*marca respectiva*/
+            break;
+        case "opJandaia":
+            /*marca respectiva*/
+            break;
+        case "opItamarity":
+            /*marca respectiva*/
+            break;
+        case "opQuero":
+            /*marca respectiva*/
+            break;
+        case "opElefante":
+            /*marca respectiva*/
+            break;
+        case "opPomarola":
+            /*marca respectiva*/
+            break;
+        case "opPinduca":
+            /*marca respectiva*/
+            break;
+        case "opSol":
+            /*marca respectiva*/
+            break;
+        case "opAnaconda":
+            /*marca respectiva*/
+            break;
+        case "opMartinelli":
+            /*marca respectiva*/
+            break;
+        case "opDouro":
+            /*marca respectiva*/
+            break;
+        case "opLider":
+            /*marca respectiva*/
+            break;
+        case "opGalo":
+            /*marca respectiva*/
+            break;
+        case "opRenata":
+            /*marca respectiva*/
+            break;
+        case "opQualy":
+            /*marca respectiva*/
+            break;
+        case "opDoriana":
+            /*marca respectiva*/
+            break;
+        case "opDelicia":
+            /*marca respectiva*/
+            break;
+        case "opLiza":
+            /*marca respectiva*/
+            break;
+        case "opPullman":
+            /*marca respectiva*/
+            break;
+        case "opCisne":
+            /*marca respectiva*/
+            break;
+        case "opCoqueiro":
+            /*marca respectiva*/
+            break;
+        case "opGomesdCosta":
+            /*marca respectiva*/
+            break;
+        case "opAmi":
+            /*marca respectiva*/
+            break;
+        case "opCastelo":
+            /*marca respectiva*/
+            break;
+        case "opIntimus":
+            /*marca respectiva*/
+            break;
+        case "opSempre":
+            /*marca respectiva*/
+            break;
+        case "opQboa":
+            /*marca respectiva*/
+            break;
+        case "opConfort":
+            /*marca respectiva*/
+            break;
+        case "opCLose":
+            /*marca respectiva*/
+            break;
+        case "opSorriso":
+            /*marca respectiva*/
+            break;
+        case "opColgate":
+            /*marca respectiva*/
+            break;
+        case "opPinho":
+            /*marca respectiva*/
+            break;
+        case "opYpe":
+            /*marca respectiva*/
+            break;
+        case "opLimpol":
+            /*marca respectiva*/
+            break;
+        case "opBombril":
+            /*marca respectiva*/
+            break;
+        case "opAssolan":
+            /*marca respectiva*/
+            break;
+        case "opOmo":
+            /*marca respectiva*/
+            break;
+        case "opBrilhante":
+            /*marca respectiva*/
+            break;
+        case "opFrancis":
+            /*marca respectiva*/
+            break;
+        case "opLux":
+            /*marca respectiva*/
+            break;
+        case "opDove":
+            /*marca respectiva*/
+            break;
+        case "opPalmolive":
+            /*marca respectiva*/
+            break;
+        case "opCrespa":
+            /*marca respectiva*/
+            break;
+        case "opImpor":
+            /*marca respectiva*/
+            break;
+        case "opCatNan":
+            /*marca respectiva*/
+            break;
+        case "opMona":
+            /*marca respectiva*/
+            break;
+        case "opNacio":
+            /*marca respectiva*/
+            break;
+        case "opTaiti":
+            /*marca respectiva*/
+            break;
+        case "opJapo":
+            /*marca respectiva*/
+            break;
+        case "opBovino":
+            /*marca respectiva*/
+            break;
+        case "opSuina":
+            /*marca respectiva*/
+            break;
+        case "opFrango":
+            /*marca respectiva*/
+            break;
+        case "opFrimesa":
+            /*marca respectiva*/
+            break;
+        case "opNula":
+            /*marca respectiva*/
+            break;
+    }
 }
