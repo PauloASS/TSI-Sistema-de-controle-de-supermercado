7d4218cd416345f2451caf8bd54ec89c73f67e13
Samukaka01 <samuca.rib0109@gmail.com>
2024-09-06 12:21:02 -0300

    nomeando value e slects e referenciando no js

---
+++ home/home.js
@@ -0,0 +1,11 @@
+const slCategorias = document.getElementById("slCategorias");
+const slMarca = document.getElementById("slMarca");
+const inPreco1 = document.getElementById("inPreco1");
+const inPreco2 = document.getElementById("inPreco2");
+const btFiltrar = document.getElementById("btFiltrar");
+const outProdutos = document.getElementById("outProdutos");
+
+btFiltrar.addEventListener("click", filtrar);
+function filtrar(){
+
+}
